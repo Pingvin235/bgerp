afd82112d02ea0b89d1928ceafeb8e8c5cfef25f
Artur Gareev <reflexive007@yandex.ru>
2020-10-02 14:44:17 +0000

    p14015 Update without FTP.

--- src/ru/bgcrm/util/distr/InstallProcessor.java
+++ src/ru/bgcrm/util/distr/InstallProcessor.java
@@ -1,8 +1,14 @@
 package ru.bgcrm.util.distr;
 
+import org.apache.commons.io.FileUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import ru.bgcrm.util.Setup;
+import ru.bgcrm.util.Utils;
+
 import java.io.File;
-import java.io.FileOutputStream;
-import java.net.URI;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -10,31 +16,22 @@ import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import org.apache.commons.net.ftp.FTP;
-import org.apache.commons.net.ftp.FTPClient;
-
-import ru.bgcrm.util.Setup;
-import ru.bgcrm.util.Utils;
-
 public class InstallProcessor {
     private static VersionInfo serverVersionInfo = VersionInfo.getVersionInfo("update");
 
     private static Setup setup = Setup.getSetup();
 
     public static final String UPDATE_TO_CHANGE_URL = "https://bgerp.org/update/";
-    private static final String UPDATE_URL = setup.get("bgerp.update.url", "ftp://ftp.bgerp.ru/pub/bgerp");
+    private static final String UPDATE_URL = setup.get("bgerp.download.url", "https://bgerp.org/download");
     private static final String TMP_DIR_PATH = Utils.getTmpDir();
 
-    private FTPClient ftp;
-
-    private Map<String, FileInfo> remoteFileMap = new HashMap<String, FileInfo>();
-    private List<FileInfo> listForInstall = new ArrayList<FileInfo>();
+    private Map<String, FileInfo> remoteFileMap = new HashMap<>();
+    private List<FileInfo> listForInstall = new ArrayList<>();
 
     private String updateVersion;
 
     public InstallProcessor(String updateVersion) {
         this.updateVersion = updateVersion;
-        connect();
         loadRemoteFileList();
     }
 
@@ -50,69 +47,49 @@ public class InstallProcessor {
         installSelected();
     }
 
-    private void connect() {
-        System.out.println("Update starting..");
-        System.out.println("Update from " + UPDATE_URL);
-
-        String kernelVersion = updateVersion;
-
-        if (Utils.isBlankString(kernelVersion)) {
-            kernelVersion = serverVersionInfo.getVersion();
-
-            if (Utils.isBlankString(kernelVersion)) {
-                System.out.println("ERROR: Can't take BGERP server version, exiting");
-                System.exit(1);
-            }
-        }
-
-        System.out.println("Version is " + kernelVersion);
-
-        URI uri = URI.create(UPDATE_URL + "/" + kernelVersion);
-        ftp = new FTPClient();
-
-        if (uri.getPort() > 0) {
-            ftp.setDefaultPort(uri.getPort());
-        }
-
-        try {
-            ftp.connect(uri.getHost());
-
-            if (System.getProperty("bginstaller.update.ftp.mode", "passive").equals("passive")) {
-                ftp.enterLocalPassiveMode();
-                System.out.println("Set passive mode..");
-            } else {
-                ftp.enterLocalActiveMode();
-                System.out.println("Set active mode..");
-            }
-
-            ftp.login("anonymous", "");
-            ftp.setFileType(FTP.BINARY_FILE_TYPE);
-
-            System.out.println("Changing dir to " + uri.getPath());
-
-            if (!ftp.changeWorkingDirectory(uri.getPath())) {
-                throw new Exception("not change to " + uri.getPath());
-            }
-        } catch (Exception e) {
-            System.out.println("ERROR: " + e.getMessage());
-            e.printStackTrace();
-            System.exit(1);
-        }
-
-        loadRemoteFileList();
-    }
-
+    /**
+     * Regexp to determine zip-files.
+     */
     private static final Pattern pattern = java.util.regex.Pattern
             .compile("^(\\w+)_((?:[\\d\\.]+)|(?:release-[a-zA-Z]+))_(\\d+)\\.zip$");
 
+    /**
+     * Loading a list of remote zip-files.
+     */
     private void loadRemoteFileList() {
+        System.out.println("Update starting..");
+        System.out.println("Update from " + UPDATE_URL);
+
         try {
-            String[] files = ftp.listNames();
-            for (String file : files) {
-                Matcher m = pattern.matcher(file);
-                if (m.find()) {
-                    FileInfo fi = new FileInfo(m.group(1), m.group(3), file);
-                    remoteFileMap.put(fi.name, fi);
+            String kernelVersion = updateVersion;
+
+            if (Utils.isBlankString(kernelVersion)) {
+                kernelVersion = serverVersionInfo.getVersion();
+
+                if (Utils.isBlankString(kernelVersion)) {
+                    System.out.println("ERROR: Can't take BGERP server version, exiting");
+                    System.exit(1);
+                }
+            }
+
+            System.out.println("Version is " + kernelVersion);
+
+            String updateUrl = String.format("%s/%s/", UPDATE_URL, kernelVersion);
+            // connecting via http(s) and parsing the page
+            Document doc = Jsoup.connect(updateUrl).get();
+
+            // iterating over the all hyperlinks
+            for (Element link : doc.select("a")) {
+                String href = link.attr("href");
+
+                // filtering only update*.zip files
+                if (href.endsWith(".zip") && href.startsWith("update_") || href.startsWith("update_lib_")) {
+                    Matcher m = pattern.matcher(href);
+
+                    if (m.find()) {
+                        FileInfo fi = new FileInfo(m.group(1), m.group(3), href, new URL( String.format( "%s%s", updateUrl, href ) ));
+                        remoteFileMap.put(fi.name, fi);
+                    }
                 }
             }
         } catch (Exception e) {
@@ -132,9 +109,7 @@ public class InstallProcessor {
                 for (FileInfo fi : listForInstall) {
                     System.out.print("Downloading " + fi.fileName);
 
-                    FileOutputStream fos = new FileOutputStream(TMP_DIR_PATH + "/" + fi.fileName);
-                    ftp.retrieveFile(fi.fileName, fos);
-                    fos.close();
+                    FileUtils.copyURLToFile(fi.url, new File(TMP_DIR_PATH + "/" + fi.fileName));
 
                     System.out.println(" OK!");
                 }
@@ -159,6 +134,10 @@ public class InstallProcessor {
         }
     }
 
+    /**
+     * Checking update necessity.
+     * @param force - version check disable.
+     */
     private void selectModulesForUpdate(boolean force) {
         List<VersionInfo> modules = VersionInfo.getInstalledVersions();
 
@@ -173,7 +152,7 @@ public class InstallProcessor {
                 continue;
             }
 
-            // билд пакета на FTP идентичен
+            // remote and local builds are identical
             if (!force && fi.build.equals(buildNumber)) {
                 continue;
             }
@@ -183,15 +162,20 @@ public class InstallProcessor {
         }
     }
 
+    /**
+     * Additional bean to store update info per each file.
+     */
     private static class FileInfo {
         public String name;
         public String build;
         public String fileName;
+        public URL url;
 
-        public FileInfo(String name, String build, String fullName) {
+        public FileInfo(String name, String build, String fullName, URL url) {
             this.name = name;
             this.build = build;
             this.fileName = fullName;
+            this.url = url;
         }
     }
 }
