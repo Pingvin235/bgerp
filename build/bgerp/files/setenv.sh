54f96619bc8bc7a2326589b01b03b07fa6bdab50
Konstantin Kudlaev <88728464+maslosan@users.noreply.github.com>
2021-11-07 15:33:55 +0000

    p14529 Deployment script.

--- build/bgerp/files/setenv.sh
+++ build/bgerp/files/setenv.sh
@@ -1,7 +1,7 @@
 #JAVA_HOME=
 
 if [ -z "$JAVA_HOME" ]; then
-  echo "The JAVA_HOME environment variable is not defined"
-  echo "This environment variable is needed to run this program"
-  exit 1
+    echo "The JAVA_HOME environment variable is not defined"
+    echo "This environment variable is needed to run this program"
+    exit 1
 fi
