61a0640d38c0bd965c52df594ae8ea3eb7d59f1d
Shamil Vakhitov <shamil@bgerp.org>
2023-06-29 13:17:14 +0000

    p14988 Fix runDynamicClass for EventListener.

--- src/ru/bgcrm/struts/action/admin/DynamicAction.java
+++ src/ru/bgcrm/struts/action/admin/DynamicAction.java
@@ -4,12 +4,14 @@ import org.apache.struts.action.ActionForward;
 import org.bgerp.custom.java.CompilationResult;
 
 import ru.bgcrm.dynamic.DynamicClassManager;
-import ru.bgcrm.event.EventProcessor;
+import ru.bgcrm.event.Event;
 import ru.bgcrm.event.RunClassRequestEvent;
+import ru.bgcrm.event.listener.EventListener;
 import ru.bgcrm.model.BGMessageException;
 import ru.bgcrm.servlet.ActionServlet.Action;
 import ru.bgcrm.struts.action.BaseAction;
 import ru.bgcrm.struts.form.DynActionForm;
+import ru.bgcrm.util.Utils;
 import ru.bgcrm.util.sql.ConnectionSet;
 
 @Action(path = "/admin/dynamic")
@@ -21,7 +23,6 @@ public class DynamicAction extends BaseAction {
         return html(conSet, form, JSP);
     }
 
-    // перекомпиляция всех файлов
     public ActionForward recompileAll(DynActionForm form, ConnectionSet conSet) throws Exception {
         CompilationResult result = DynamicClassManager.getInstance().recompileAll();
 
@@ -30,13 +31,16 @@ public class DynamicAction extends BaseAction {
         return html(conSet, form, JSP);
     }
 
+    // TODO: Is actively used, preserve when migrating to Custom.
     public ActionForward runDynamicClass(DynActionForm form, ConnectionSet conSet) throws Exception {
         String className = form.getParam("class");
 
         String ifaceType = form.getParam("iface", "event");
-        // запуск интерфейса EventListener
-        if ("event".equals(ifaceType))
-            EventProcessor.processEvent(new RunClassRequestEvent(form), conSet);
+        if ("event".equals(ifaceType)) {
+            @SuppressWarnings("unchecked")
+            EventListener<Event> listener = (EventListener<Event>) Utils.newInstance(className);
+            listener.notify(new RunClassRequestEvent(form), conSet);
+        }
         // запуск интерфейса Runnable
         else {
             Class<?> clazz = null;
