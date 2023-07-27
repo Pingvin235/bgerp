121515d262385cc9465ccfbd7371d6d9fc38f9ff
Shamil Vakhitov <shamil@bgerp.org>
2023-07-27 15:24:03 +0000

    p14747 Extended BGBillingTest.

--- src/ru/bgcrm/plugin/bgbilling/dao/Tables.java
+++
@@ -1,6 +0,0 @@
-package ru.bgcrm.plugin.bgbilling.dao;
-
-public class Tables
-{
-	public static final String TABLE_COMMON_CONTRACT = "bgbilling_common_contract";
-}
--- srcx/itest/org/bgerp/itest/plugin/bgbilling/BGBillingTest.java
+++ srcx/itest/org/bgerp/itest/plugin/bgbilling/BGBillingTest.java
@@ -8,15 +8,19 @@ import org.bgerp.itest.helper.ParamHelper;
 import org.bgerp.itest.helper.ProcessHelper;
 import org.bgerp.itest.helper.ResourceHelper;
 import org.bgerp.itest.helper.UserHelper;
+import org.bgerp.itest.kernel.db.DbTest;
 import org.bgerp.itest.kernel.process.ProcessTest;
 import org.bgerp.itest.kernel.user.UserTest;
 import org.testng.annotations.Test;
 
+import ru.bgcrm.dao.process.ProcessLinkDAO;
 import ru.bgcrm.model.param.Parameter;
 import ru.bgcrm.model.process.Process;
 import ru.bgcrm.model.process.ProcessGroup;
+import ru.bgcrm.model.process.ProcessLink;
 import ru.bgcrm.model.process.TypeProperties;
 import ru.bgcrm.plugin.bgbilling.Plugin;
+import ru.bgcrm.plugin.bgbilling.proto.model.Contract;
 
 @Test(groups = "bgbilling", priority = 100, dependsOnGroups = { "process", "message" })
 public class BGBillingTest {
@@ -24,11 +28,15 @@ public class BGBillingTest {
     private static final String TITLE = PLUGIN.getTitleWithPrefix();
     private static final String TITLE_HD = TITLE + " HD";
 
+    private static final String BILLING_ID = "bill";
+
+    private int processTypeId;
+
     private int paramHdCostId;
     private int paramHdStatusId;
     private int paramHdAutoCloseId;
 
-    private int processTypeHdId;
+    private int processHdTypeId;
 
     @Test
     public void param() throws Exception {
@@ -41,25 +49,33 @@ public class BGBillingTest {
 
     @Test(dependsOnMethods = "param")
     public void processType() throws Exception {
+        processTypeId = ProcessHelper.addType(TITLE, ProcessTest.processTypeTestGroupId, true, null).getId();
+
         var props = new TypeProperties();
-        props.setStatusIds(List.of(ProcessTest.statusOpenId, /* ProcessTest.statusProgressId, */ProcessTest.statusDoneId));
+        props.setStatusIds(List.of(ProcessTest.statusOpenId, ProcessTest.statusDoneId));
         props.setCreateStatus(ProcessTest.statusOpenId);
         props.setCloseStatusIds(Set.of(ProcessTest.statusDoneId));
         props.setParameterIds(List.of(paramHdCostId, paramHdStatusId, paramHdAutoCloseId));
         props.setGroups(ProcessGroup.toProcessGroupSet(Set.of(UserTest.USER_GROUP_ADMIN_ID), 0));
-        // props.setConfig(ResourceHelper.getResource(this, "process.type.hd.config.txt"));
 
-        processTypeHdId = ProcessHelper.addType(TITLE_HD, ProcessTest.processTypeTestGroupId, false, props).getId();
+        processHdTypeId = ProcessHelper.addType(TITLE_HD, processTypeId, false, props).getId();
     }
 
     @Test(dependsOnMethods = "processType")
     public void processQueue() throws Exception {
-        int queueId = ProcessHelper.addQueue(TITLE_HD,
+        int queueId = ProcessHelper.addQueue(TITLE,
                 ConfigHelper.generateConstants(
+                    "BILLING_ID", BILLING_ID
+                ) + ResourceHelper.getResource(this, "process.queue.config.txt"), Set.of(processTypeId));
+        UserHelper.addUserProcessQueues(UserTest.USER_ADMIN_ID, Set.of(queueId));
+
+        queueId = ProcessHelper.addQueue(TITLE_HD,
+                ConfigHelper.generateConstants(
+                    "BILLING_ID", BILLING_ID,
                     "PARAM_HD_COST_ID", paramHdCostId,
                     "PARAM_HD_STATUS_ID", paramHdStatusId
-                ) + ResourceHelper.getResource(this, "process.queue.hd.config.txt"),
-                Set.of(processTypeHdId));
+                ) + ResourceHelper.getResource(this, "process.hd.queue.config.txt"),
+                Set.of(processHdTypeId));
         UserHelper.addUserProcessQueues(UserTest.USER_ADMIN_ID, Set.of(queueId));
     }
 
@@ -67,7 +83,8 @@ public class BGBillingTest {
     public void config() throws Exception {
         ConfigHelper.addIncludedConfig(PLUGIN,
             ConfigHelper.generateConstants(
-                "PROCESS_TYPE_HD_ID", processTypeHdId,
+                "BILLING_ID", BILLING_ID,
+                "PROCESS_HD_TYPE_ID", processHdTypeId,
                 "PARAM_HD_COST_ID", paramHdCostId,
                 "PARAM_HD_STATUS_ID", paramHdStatusId,
                 "PARAM_HD_AUTO_CLOSE_ID", paramHdAutoCloseId,
@@ -75,5 +92,13 @@ public class BGBillingTest {
                 "PROCESS_HD_CLOSE_STATUS_ID", ProcessTest.statusDoneId,
                 "PROCESS_HD_READ_STATUS_IDS", ""
             ) + ResourceHelper.getResource(this, "config.txt"));
+    }
+
+    @Test(dependsOnMethods = "processType")
+    public void process() throws Exception {
+        int processId = ProcessHelper.addProcess(processTypeId, UserTest.USER_ADMIN_ID, TITLE).getId();
+        new ProcessLinkDAO(DbTest.conRoot).addLinkIfNotExist(new ProcessLink(processId, Contract.OBJECT_TYPE + ":" + BILLING_ID, 455, "x0000"));
+
+        // HD processes have to be imported
     }
 }
