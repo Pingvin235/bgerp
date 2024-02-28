f7ea4b69667d1b3933502e7d5a4a91d16ed2533c
BGERP <bgerpp@gmail.com>
2024-02-28 13:45:40 +0000

    p15060 Fixed dump scripts.

--- build/bgerp/files/dump_heap.sh
+++ build/bgerp/files/dump_heap.sh
@@ -4,14 +4,14 @@ cd ${0%${0##*/}}.
 
 . ./setenv.sh
 
-PID=`cat ./.run/bgcrm.pid`
+PID=`cat ./.run/bgerp.pid`
 
 if [ -z "$1" ]; then
     echo "Dump name is not defined"
     exit 1
 fi
 
-./crm.sh gc
+./erp.sh gc
 
 sleep 5
 
--- build/bgerp/files/dump_stack.sh
+++ build/bgerp/files/dump_stack.sh
@@ -4,11 +4,11 @@ cd ${0%${0##*/}}.
 
 . ./setenv.sh
 
-PID=`cat ./.run/bgcrm.pid`
+PID=`cat ./.run/bgerp.pid`
 
 if [ -z "$1" ]; then
     echo "Dump name is not defined"
     exit 1
 fi
 
-$JAVA_HOME/bin/jstack  $PID > $1 
+$JAVA_HOME/bin/jstack  $PID > $1
