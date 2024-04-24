79d25b9952b4179e42b1e7fd13864eea609da254
BGERP <bgerpp@gmail.com>
2024-04-24 13:31:03 +0000

    p15086 Delete outdated itest config

--- srcx/itest/org/bgerp/itest/kernel/config/ConfigTest.java
+++ srcx/itest/org/bgerp/itest/kernel/config/ConfigTest.java
@@ -5,7 +5,6 @@ import java.util.Date;
 import org.bgerp.itest.helper.ConfigHelper;
 import org.bgerp.itest.helper.ResourceHelper;
 import org.bgerp.itest.kernel.db.DbTest;
-import org.bgerp.itest.kernel.user.UserTest;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -18,7 +17,6 @@ public class ConfigTest {
     public static final int ROLE_FOLLOW_ID = 1;
 
     public static volatile int configMainId;
-    public static volatile int configProcessNotificationId;
 
     @Test(groups = "config", dependsOnGroups = "dbInit")
     public void initMainConfig() throws Exception {
@@ -37,20 +35,4 @@ public class ConfigTest {
 
         dao.setActiveGlobalConfig(configMainId);
     }
-
-    @Test(groups = "configProcessNotification", dependsOnGroups = { "param", "message" })
-    public void initProcessNotificationConfig() throws Exception {
-        var con = DbTest.conRoot;
-        var dao = new ConfigDAO(con);
-
-        var config = ConfigHelper.createConfig("Process Notification",
-            ConfigHelper.generateConstants("PARAM_USER_EMAIL_ID", UserTest.paramEmailId) +
-            ResourceHelper.getResource(this, "config.process.notification.txt"));
-
-        dao.updateGlobalConfig(config);
-
-        configProcessNotificationId = config.getId();
-        Assert.assertTrue(configProcessNotificationId > 0);
-    }
-
 }
