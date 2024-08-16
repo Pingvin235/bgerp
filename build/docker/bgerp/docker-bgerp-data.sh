10e167f410a5d9cb4e5b0606f7648eb14161f38d
BGERP <bgerpp@gmail.com>
2024-08-16 11:08:45 +0000

    p15102 Docker image without update to Master and Demo content

--- build/docker/bgerp/docker-bgerp-data.sh
+++ build/docker/bgerp/docker-bgerp-data.sh
@@ -21,18 +21,26 @@ if [ ! -d "/var/lib/mysql/mysql" ]; then
     mysql --default-character-set=utf8 -uroot -p$MYSQL_ROOT_PASSWORD < /opt/bgerp/db_create.sql
     mysql --default-character-set=utf8 -ubgerp -p$ERP_DB_PWD bgerp < /opt/bgerp/db_init.sql
 
-    # install master release
-    /opt/bgerp/installer.sh installc 0
+    if [ "$MASTER" != "no" ]; then
+        echo "Installing Master release"
+        /opt/bgerp/installer.sh installc 0
+    else
+        echo "Skipping installation Master release"
+    fi
 
     TMP_DIR=/tmp/bgerp
 
-    echo "Applying DEMO database"
-    wget https://demo.bgerp.org/bgerp.sql -O $TMP_DIR/bgerp.sql
-    mysql --default-character-set=utf8 -ubgerp -p$ERP_DB_PWD bgerp < $TMP_DIR/bgerp.sql
+    if [ "$DEMO" != "no" ]; then
+        echo "Applying DEMO database"
+        wget https://demo.bgerp.org/bgerp.sql -O $TMP_DIR/bgerp.sql
+        mysql --default-character-set=utf8 -ubgerp -p$ERP_DB_PWD bgerp < $TMP_DIR/bgerp.sql
 
-    echo "Applying DEMO filestorage"
-    wget https://demo.bgerp.org/filestorage.zip -O $TMP_DIR/filestorage.zip
-    unzip $TMP_DIR/filestorage.zip -d /opt/bgerp/filestorage
+        echo "Applying DEMO filestorage"
+        wget https://demo.bgerp.org/filestorage.zip -O $TMP_DIR/filestorage.zip
+        unzip $TMP_DIR/filestorage.zip -d /opt/bgerp/filestorage
+    else
+        echo "Skipping DEMO database and filestorage"
+    fi
 
     echo "Downloading DEMO license"
     wget https://bgerp.org/download/lic.data -O /opt/bgerp/lic.data
