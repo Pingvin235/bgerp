7faecb842d0979822a56a75410ea8ae8f8fec702
BGERP <bgerpp@gmail.com>
2025-06-24 18:32:33 +0200

    p14874 Process event in current thread

--- src/org/bgerp/app/event/EventProcessor.java
+++ src/org/bgerp/app/event/EventProcessor.java
@@ -4,20 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 
-import org.bgerp.app.cfg.Setup;
 import org.bgerp.app.event.iface.Event;
 import org.bgerp.app.event.iface.EventListener;
 import org.bgerp.app.exception.BGMessageException;
 import org.bgerp.util.Log;
-
-import com.google.common.util.concurrent.ThreadFactoryBuilder;
 
 import ru.bgcrm.util.sql.ConnectionSet;
 
@@ -30,7 +21,6 @@ public class EventProcessor {
     private static final Log log = Log.getLog();
 
     private static final Map<Class<?>, List<PrioritizedListener>> SUBSCRIBERS = new ConcurrentHashMap<>();
-    private static final ExecutorService EXECUTOR = Executors.newCachedThreadPool(new ThreadFactoryBuilder().setNameFormat("EventProcessor-%d").build());
 
     /**
      * Subscribes a listener to events of a class.
@@ -86,32 +76,7 @@ public class EventProcessor {
             for (var item : listeners) {
                 if (!event.processing())
                     break;
-                processEvent(event, item.listener(), conSet);
+                item.listener().notify(event, conSet);
             }
-    }
-
-    private static void processEvent(Event event, EventListener<Event> listener, ConnectionSet conSet) throws Exception {
-        final long timeout = Setup.getSetup().getLong("event.process.timeout", 5000L);
-        try {
-            if (!isDebugMode()) {
-                Future<byte[]> future = EXECUTOR.submit(new RequestTask(conSet, event, listener));
-                future.get(timeout, TimeUnit.MILLISECONDS);
-            } else {
-                listener.notify(event, conSet);
-            }
-        } catch (TimeoutException e) {
-            log.error("Timeout {} ms was exceeded in listener {}", timeout, listener.getClass().getName());
-        } catch (InterruptedException | ExecutionException e) {
-            var cause = e.getCause();
-            if (!(cause instanceof BGMessageException)) {
-                log.error("In listener {} occurred an exception: {}", listener.getClass().getName(), e.getMessage());
-                log.error(e);
-            }
-            throw (Exception) cause;
-        }
-    }
-
-    private static boolean isDebugMode() {
-        return java.lang.management.ManagementFactory.getRuntimeMXBean().getInputArguments().toString().indexOf("jdwp") >= 0;
     }
 }
