f55f58451d49c9abdb007fd28247e2449784b4c0
BGERP <bgerpp@gmail.com>
2025-09-08 19:11:33 +0500

    p15183 Rewritten PatternFormatter

--- src/org/bgerp/util/text/PatternFormatter.java
+++ src/org/bgerp/util/text/PatternFormatter.java
@@ -8,53 +8,68 @@ import java.util.regex.Pattern;
 import ru.bgcrm.util.Utils;
 
 /**
- * Processor for substitution patterns like <pre>(${key})(текст ${key1})}</pre>.
+ * Processor for substitution patterns like <pre>${key} text (text1 ${key1})}</pre>.
  * Braces bound not mandatory area, being added only when for the key inside has defined a value.
  *
  * @author Shamil Vakhitov
  */
 public class PatternFormatter {
-    private static final String BEFORE_VAR = "\\(([\\wа-яА-Я\\,\\.\\s\\[\\]\\\\/#\\(\\)]*)\\$\\{";
-    private static final String AFTER_VAR = "\\}([\\wа-яА-Я\\,\\.\\s\\[\\]\\\\/#\\(\\)]*)\\)";
-    private static final Pattern VAR_PATTERN = Pattern.compile(BEFORE_VAR + "([\\w:]+)" + AFTER_VAR);
+    private static final Pattern VAR_PATTERN = Pattern.compile("\\$\\{([\\w:]+)\\}");
 
     /**
-     * Executes substitutions in a pattern.
-     * @param pattern the pattern.
-     * @param processor values provider for found variables, provided {@code null} values treated as empty strings.
-     * @return pattern with applied substitutions.
+     * Executes substitutions in a pattern
+     * @param pattern the pattern
+     * @param processor values provider for found variables, provided {@code null} values treated as empty strings
+     * @return the pattern with applied substitutions
      */
     public static String processPattern(String pattern, Function<String, String> processor) {
         StringBuilder result = new StringBuilder(pattern.length());
 
         int pos = 0;
 
-        Matcher m = VAR_PATTERN.matcher(pattern);
-        while (m.find()) {
-            result.append(pattern.substring(pos, m.start()));
+        final Matcher m = VAR_PATTERN.matcher(pattern);
+        while (pos < pattern.length() && m.find(pos)) {
+            final int start = m.start();
+            int brace = pattern.indexOf('(', pos, start);
+            final boolean braces = pos <= brace;
 
-            String value = processor.apply(m.group(2));
-            if (Utils.notBlankString(value)) {
-                String prefix = m.group(1);
-                if (prefix.startsWith(",") && result.length() == 0) {
-                    prefix = prefix.substring(1);
-                }
-                result.append(prefix).append(value).append(m.group(3));
+            result.append(pattern.substring(pos, braces ? brace : start));
+
+            String name = m.group(1);
+            String value = processor.apply(name);
+            final boolean valueExists = Utils.notBlankString(value);
+
+            if (valueExists) {
+                if (braces)
+                    result.append(pattern.substring(brace + 1, start));
+
+                result.append(value);
             }
 
-            pos = m.end();
+            final int end = m.end() - 1;
+            if (braces) {
+                brace = pattern.indexOf(')', end);
+                if (end < brace) {
+                    pos = brace + 1;
+                    if (valueExists)
+                        result.append(pattern.substring(end + 1, brace));
+                } else
+                    pos = end + 1;
+            } else
+                pos = end + 1;
         }
 
-        result.append(pattern.substring(pos));
+        if (pos < pattern.length())
+            result.append(pattern.substring(pos));
 
         return result.toString();
     }
 
     /**
-     * Executes substitutions in a pattern.
-     * @param pattern the pattern.
-     * @param values variable values map, provided {@code null} values treated as empty strings.
-     * @return pattern with applied substitutions.
+     * Executes substitutions in a pattern
+     * @param pattern the pattern
+     * @param values variable values map, provided {@code null} values treated as empty strings
+     * @return the pattern with applied substitutions
      */
     public static String processPattern(String pattern, Map<String, String> values) {
         return processPattern(pattern, key -> values.get(key));
--- srcx/test/org/bgerp/util/text/PatternFormatterTest.java
+++ srcx/test/org/bgerp/util/text/PatternFormatterTest.java
@@ -6,12 +6,11 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class PatternFormatterTest {
-    private static final String PATTERN = "(x ${key1}) тест ( ${key2})";
-    private static final String PATTERN_COLUMN = "(x ${key1:extra1}) тест ( ${key2:extra2})";
-
     @Test
-    public void testProcessPattern() {
-        String value = PatternFormatter.processPattern(PATTERN, variable -> {
+    public void testProcessPatternSimple() {
+        final String pattern = "(x ${key1}) тест ( ${key2})";
+
+        String value = PatternFormatter.processPattern(pattern, variable -> {
             if ("key1".equals(variable))
                 return "value1";
             if ("key2".equals(variable))
@@ -20,14 +19,125 @@ public class PatternFormatterTest {
         });
         Assert.assertEquals("x value1 тест ", value);
 
-        value = PatternFormatter.processPattern(PATTERN, Map.of("key1", "value1", "key2", "value2"));
+        value = PatternFormatter.processPattern(pattern, Map.of("key1", "value1", "key2", "value2"));
         Assert.assertEquals("x value1 тест  value2", value);
+    }
 
-        value = PatternFormatter.processPattern(PATTERN_COLUMN, variable -> {
+    @Test
+    public void testProcessPatternSemicolon() {
+        final String pattern = "(x ${key1:extra1}) тест ( ${key2:extra2}) end";
+
+        String value = PatternFormatter.processPattern(pattern, variable -> {
             if (variable.startsWith("key1"))
                 return "value1";
             return variable;
         });
-        Assert.assertEquals("x value1 тест  key2:extra2", value);
+        Assert.assertEquals("x value1 тест  key2:extra2 end", value);
+    }
+
+    @Test
+    public void testProcessPatternUnderscore() {
+        final String pattern = "(x ${key_1}) bla  ${key_2}";
+
+        String value = PatternFormatter.processPattern(pattern, variable -> {
+            if ("key_1".equals(variable))
+                return "value1";
+            if ("key_2".equals(variable))
+                return "value2";
+            return "???";
+        });
+        Assert.assertEquals("x value1 bla  value2", value);
+    }
+
+    @Test
+    public void testProcessPatternEmptyVars() {
+        final String pattern = "(x ${key_1}) bla  ${key_2}";
+
+        String value = PatternFormatter.processPattern(pattern, variable -> {
+            if ("key_1".equals(variable))
+                return null;
+            if ("key_2".equals(variable))
+                return "";
+            return "???";
+        });
+        Assert.assertEquals(" bla  ", value);
+    }
+
+    @Test
+    public void testProcessPatternCommas() {
+        final String pattern = "(${key_1})(, ${key_2})(, ${key_3})";
+
+        String value = PatternFormatter.processPattern(pattern, variable -> {
+            if ("key_1".equals(variable))
+                return null;
+            if ("key_2".equals(variable))
+                return "value2";
+            if ("key_3".equals(variable))
+                return "value3";
+            return "???";
+        });
+        Assert.assertEquals(", value2, value3", value);
+    }
+
+    @Test
+    public void testProcessPatternOrgTitle() {
+        final String pattern = "${param_33}( Бла-бла: ${param_45})";
+
+        String value = PatternFormatter.processPattern(pattern, variable -> {
+            if ("param_33".equals(variable))
+                return "ИП";
+            if ("param_45".equals(variable))
+                return "Образцов И.И.";
+            return "???";
+        });
+        Assert.assertEquals("ИП Бла-бла: Образцов И.И.", value);
+    }
+
+    @Test
+    public void testProcessPatternAddress() {
+        final String pattern = "(${street})(, ${house})(, ${floor} floor)(, apt. ${flat})( ${room})( ${comment})( ${index})( ${city})( [${comment}])";
+
+        String value = PatternFormatter.processPattern(pattern, variable -> {
+            if ("index".equals(variable))
+                return "450103";
+            if ("city".equals(variable))
+                return "Уфа";
+            if ("street".equals(variable))
+                return "Габдуллы Амантая";
+            if ("house".equals(variable))
+                return "6";
+            if ("flat".equals(variable))
+                return "33";
+            return "";
+        });
+        Assert.assertEquals("Габдуллы Амантая, 6, apt. 33 450103 Уфа", value);
+    }
+
+    @Test
+    public void testProcessPatternPhone() {
+        final String pattern = "(${number})( [${comment}])";
+
+        String value = PatternFormatter.processPattern(pattern, variable -> {
+            if ("number".equals(variable))
+                return "42";
+            if ("comment".equals(variable))
+                return "Comment";
+            return "";
+        });
+        Assert.assertEquals("42 [Comment]", value);
+    }
+
+    @Test
+    public void testProcessPatternOrgName() {
+        final String pattern = "${param_1} \"${param_2}\"";
+
+        String value = PatternFormatter.processPattern(pattern, variable -> {
+            if ("param_1".equals(variable))
+                return "ИП";
+            if ("param_2".equals(variable))
+                return "Образцов Иван";
+            return "";
+        });
+        Assert.assertEquals("ИП \"Образцов Иван\"", value);
     }
 }
