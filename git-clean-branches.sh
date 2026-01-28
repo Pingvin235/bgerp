b8804699d890efdcfc852374aae128bdda0dd86e
BGERP <bgerpp@gmail.com>
2026-01-28 10:21:47 +0100

    p15203 Extend git-clean-branches.sh script

--- git-clean-branches.sh
+++ git-clean-branches.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+# cleanup all references to non-existent remote branches
+git remote prune origin
+# cleanup local branches that no longer exist on remote
 git branch -vv | grep -P '^\s+p' | grep 'gone]' | awk '{print $1}' | xargs git branch -D
+# cleanup the repo from unreferenced objects
+git reflog expire --expire-unreachable=now --all && git gc --prune=now
+# cleanup unused GIT LFS files
+git lfs prune
