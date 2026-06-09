b365cd9a629f8cea806fab11fffc95f7c672fe95
BGERP <bgerpp@gmail.com>
2026-06-09 15:41:10 +0200

    p15204 Delete deprecated BaseAction classes

--- src/org/bgerp/action/BaseAction.java
+++
@@ -1,8 +0,0 @@
-package org.bgerp.action;
-
-/**
- * The class made for backward compatibility
- */
-@Deprecated
-public abstract class BaseAction extends org.bgerp.action.base.BaseAction {
-}
--- src/ru/bgcrm/struts/action/BaseAction.java
+++
@@ -1,8 +0,0 @@
-package ru.bgcrm.struts.action;
-
-/**
- * The class made for backward compatibility
- */
-@Deprecated
-public class BaseAction extends org.bgerp.action.BaseAction {
-}
