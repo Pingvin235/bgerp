cd7dc7ceca7c36b799fea82aec9af447cf9bf6a1
BGERP <bgerpp@gmail.com>
2026-06-10 18:15:34 +0200

    p15204 ReportAction pathId

--- src/org/bgerp/plugin/report/action/ReportAction.java
+++ src/org/bgerp/plugin/report/action/ReportAction.java
@@ -2,22 +2,22 @@ package org.bgerp.plugin.report.action;
 
 import org.apache.struts.action.ActionForward;
 import org.bgerp.action.base.BaseAction;
-import org.bgerp.app.exception.BGException;
 import org.bgerp.plugin.report.Config;
 import org.bgerp.plugin.report.Plugin;
 import org.bgerp.plugin.report.model.Report;
 
+import javassist.NotFoundException;
 import ru.bgcrm.servlet.ActionServlet.Action;
 import ru.bgcrm.struts.form.DynActionForm;
 import ru.bgcrm.util.Utils;
 import ru.bgcrm.util.sql.ConnectionSet;
 
 /**
- * Action for supporting JSP reports.
+ * Action for supporting JSP reports
  *
  * @author Shamil Vakhitov
  */
-@Action(path = "/user/plugin/report/report")
+@Action(path = "/user/plugin/report/report", pathId = true)
 public class ReportAction extends BaseAction {
 
     @Override
@@ -29,10 +29,10 @@ public class ReportAction extends BaseAction {
         return html(conSet, form, Plugin.PATH_JSP_USER + "/report.jsp");
     }
 
-    public ActionForward get(DynActionForm form, ConnectionSet conSet) throws Exception {
+    public ActionForward get(DynActionForm form, ConnectionSet conSet) throws NotFoundException {
         Report report = setup.getConfig(Config.class).getReportMap().get(form.get("reportId"));
         if (report == null)
-            throw new BGException("Report not found");
+            throw new NotFoundException("Report not found");
 
         // deprecated, but already used in existing reports
         form.setForwardFile(report.getJsp());
--- webapps/WEB-INF/jspf/user/plugin/report/menu_items.jsp
+++ webapps/WEB-INF/jspf/user/plugin/report/menu_items.jsp
@@ -2,5 +2,5 @@
 <%@ include file="/WEB-INF/jspf/taglibs.jsp"%>
 
 <ui:menu-group title="${l.l('Reports')}" icon="ti-stats-up" actionFactory="org.bgerp.plugin.report.action.ReportActionBase$Factory">
-	<ui:menu-item title="${l.l('JSP Reports')}" href="report" action="org.bgerp.plugin.report.action.ReportAction:null" />
+	<ui:menu-item title="${l.l('JSP Reports')}" href="report" action="/user/plugin/report/report:null" />
 </ui:menu-group>
