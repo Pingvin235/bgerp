ba28ce69bef6a6485a9ad8907a039d57e691e482
BGERP <bgerpp@gmail.com>
2026-07-21 14:41:54 +0200

    p15190 Format DocumentGenerateEvent

--- src/ru/bgcrm/plugin/document/event/DocumentGenerateEvent.java
+++ src/ru/bgcrm/plugin/document/event/DocumentGenerateEvent.java
@@ -9,75 +9,61 @@ import ru.bgcrm.plugin.document.model.Pattern;
 import ru.bgcrm.struts.form.DynActionForm;
 import ru.bgcrm.util.Utils;
 
-public class DocumentGenerateEvent
-	extends UserEvent
-{
-	private final Pattern pattern;
-	private final String objectType;
-	private final List<Integer> objectIds;
+public class DocumentGenerateEvent extends UserEvent {
+    private final Pattern pattern;
+    private final String objectType;
+    private final List<Integer> objectIds;
 
-	private Document resultDocument;
-	private byte[] resultBytes;
+    private Document resultDocument;
+    private byte[] resultBytes;
 
-	public DocumentGenerateEvent( DynActionForm form, Pattern pattern,
-	                              String objectType, List<Integer> objectIds)
-	{
-		super( form );
-		this.pattern = pattern;
-		this.objectType = objectType;
-		this.objectIds = objectIds;
-	}
+    public DocumentGenerateEvent(DynActionForm form, Pattern pattern, String objectType, List<Integer> objectIds) {
+        super(form);
+        this.pattern = pattern;
+        this.objectType = objectType;
+        this.objectIds = objectIds;
+    }
 
-	public Document setResultDocument()
-	{
-		resultDocument = new Document();
-		resultDocument.setObjectId( Utils.getFirst( objectIds ) );
-		resultDocument.setObjectType( objectType );
-		return resultDocument;
-	}
+    public Document setResultDocument() {
+        resultDocument = new Document();
+        resultDocument.setObjectId(Utils.getFirst(objectIds));
+        resultDocument.setObjectType(objectType);
+        return resultDocument;
+    }
 
-	public Document getResultDocument()
-	{
-		return resultDocument;
-	}
+    public Document getResultDocument() {
+        return resultDocument;
+    }
 
-	public byte[] getResultBytes()
-	{
-		return resultBytes;
-	}
+    public byte[] getResultBytes() {
+        return resultBytes;
+    }
 
-	public void setResultBytes( byte[] resultStream )
-	{
-		this.resultBytes = resultStream;
-	}
+    public void setResultBytes(byte[] resultStream) {
+        this.resultBytes = resultStream;
+    }
 
-	public Pattern getPattern()
-	{
-		return pattern;
-	}
+    public Pattern getPattern() {
+        return pattern;
+    }
 
-	public String getObjectType()
-	{
-		return objectType;
-	}
+    public String getObjectType() {
+        return objectType;
+    }
 
-	public int getObjectId()
-	{
-		return Utils.getFirst( objectIds );
-	}
+    public int getObjectId() {
+        return Utils.getFirst(objectIds);
+    }
 
-	public List<Integer> getObjectIds()
-	{
-		return objectIds;
-	}
+    public List<Integer> getObjectIds() {
+        return objectIds;
+    }
 
-	public boolean isDebug()
-	{
-		return isDebug(form);
-	}
+    public boolean isDebug() {
+        return isDebug(form);
+    }
 
-	public static boolean isDebug(DynActionForm form)
-	{
-		return form.getParamBoolean( "debug");
-	}
+    public static boolean isDebug(DynActionForm form) {
+        return form.getParamBoolean("debug");
+    }
 }
